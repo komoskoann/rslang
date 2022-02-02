c3645fb2c3db175be27b514ac3643f25ebab5239
komoskoann\\----------- <komoskoann@gmail.com>
2022-02-02 23:06:17 +0300

    feat: delete footer in mini-games

--- src/application/gamesPage/sprintGameSection.ts
+++ src/application/gamesPage/sprintGameSection.ts
@@ -1,7 +1,14 @@
 import Control from '../../controls/control';
+import Footer from '../mainPage/footer';
 
 export default class SprintGameSection extends Control {
   constructor(parentNode: HTMLElement) {
     super(parentNode, 'section', 'sprint-game-section', 'test sprint-game');
+    document.body.lastChild.remove();
+  }
+
+  destroy() {
+    super.destroy();
+    new Footer(document.body);
   }
 }
