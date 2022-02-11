87c91a99cf313dfcc863cfc0f532862dd892dfbf
m9t3zhn1k <m9t3zhn1k@mail.ru>
2022-02-11 22:07:44 +0300

    add authoriz form available for opening from every page, fix bug with avatar, add uppercase to name while registration

--- src/application/autorizationForm/autorizationForm.ts
+++ src/application/autorizationForm/autorizationForm.ts
@@ -40,6 +40,16 @@ export default class AuthorizationForm extends Control {
     this.autorizationComponent = autorizationHTML;
     this.registrationComponent = registrationHTML;
     this.autorizationService = new AutorizationService();
+    this.addAvatar();
+  }
+
+  private addAvatar() {
+    if (JSON.parse(localStorage.getItem('currentUser'))) {
+      this.avatar = new Avatar(
+        document.querySelector('.authorizationWrapper'),
+        JSON.parse(localStorage.getItem('currentUser'))?.name[0],
+      );
+    }
   }
 
   private searchOpenAuthorizationButton = (): HTMLButtonElement => document.querySelector('.login');
@@ -157,12 +167,15 @@ export default class AuthorizationForm extends Control {
       this.closeAuthorizationForm();
       console.log(content);
     }
-    this.avatar = new Avatar(document.querySelector('.authorizationWrapper'), app.currentUser.name[0]);
+    this.addAvatar();
   };
 
   private signUpAction = async (e: Event): Promise<void> => {
     e.preventDefault();
-    const name = (document.getElementById('authorizationFormInputName') as HTMLInputElement).value;
+    let name = (document.getElementById('authorizationFormInputName') as HTMLInputElement).value
+      .split(' ')
+      .map((item) => item.slice(0, 1).toUpperCase() + item.slice(1))
+      .join(' ');
     const email = (document.getElementById('authorizationFormInputEmail') as HTMLInputElement).value;
     const password = (document.getElementById('authorizationFormInputPassword') as HTMLInputElement).value;
     const response = await this.autorizationService.signUp({ email, password, name });
