9f944a3dfa010d1b44154f018d0f04b7746b892b
marinchik99 <marina_1999@tut.by>
2022-02-22 15:03:21 +0300

    feat: add listen to results game, fix buttons click before start

--- src/application/gamesPage/sprintGame/sprintGameCard.ts
+++ src/application/gamesPage/sprintGame/sprintGameCard.ts
@@ -6,6 +6,12 @@ import MainSprintSection from './sprintMainSection';
 import LocalStorage from '../../services/words/localStorage';
 
 export default class SprintGameCard extends Control {
+  private wordCardInfo: ISprint;
+
+  private audio: HTMLAudioElement;
+
+  private serverURL: string = 'https://rslangapplication.herokuapp.com/';
+
   mainSprint: MainSprintSection;
 
   service: GetWordsToSprint = new GetWordsToSprint();
@@ -15,8 +21,12 @@ export default class SprintGameCard extends Control {
   englishLevelBook : number;
 
   englishPageBook : number;
+  
+  private isPlaying: boolean = false;
 
   gameFrom: string;
+
+  private playNum: number = 0;
 
   localStorage: LocalStorage = new LocalStorage();
 
@@ -40,12 +50,13 @@ export default class SprintGameCard extends Control {
 
   private wrongAnswers: number;
 
+  private isHard: boolean;
+
   constructor(parentNode: HTMLElement) {
     super(parentNode, 'section', 'sprint-card-section');
     const cardSprint = new Control(this.node, 'div', 'card-sprint container-xxl');
     cardSprint.node.innerHTML = sprintGameCard;
     this.currentPage = this.randomPage();
-    console.log(this.currentPage);
     this.startTime();
     this.englishPageBook =
       +this.localStorage.getFromLocalStorage('currentWordPage');
@@ -55,17 +66,16 @@ export default class SprintGameCard extends Control {
       +this.localStorage.getFromLocalStorage('EngLevel');
     this.maxSeries = Math.max.apply(null, this.seriesArr);
     this.getFrom()
+   
   }
 
   private getFrom() {
     this.gameFrom = this.localStorage.getFromLocalStorage('from');
     if (this.gameFrom === 'game'){
       this.getWords(this.currentPage, this.currentEnglishLevel);
-      console.log(this.currentPage, this.currentEnglishLevel)
     }
     else if (this.gameFrom === 'ebook'){
       this.getWords(this.englishPageBook,this.englishLevelBook);
-      console.log(this.englishLevelBook, this.englishPageBook)
     }
     
   }
@@ -76,33 +86,32 @@ export default class SprintGameCard extends Control {
 
   async getWords(page: number, group: number): Promise<void> {
     let wordsOnPage: ISprint[] = await this.service.getWordstoSprint(page, group);
-    console.log(wordsOnPage)
     let shuffled = wordsOnPage.map((value) => ({value})).sort(() => Math.random() - 0.5).map(({value}) => value);
-    this.node.querySelector('.word-translation').innerHTML = shuffled[0].wordTranslate;
+    let correctArr = shuffled.slice(0, 8);
+    this.node.querySelector('.word-translation').innerHTML = (correctArr.includes(wordsOnPage[0])) ? wordsOnPage[0].wordTranslate : shuffled[0].wordTranslate;
     this.node.querySelector('.word-name').innerHTML = wordsOnPage[0].word;
-    let j = 1;
-
+    let j = 1; 
     this.node.querySelectorAll('[word]').forEach((word) => {
       function createResult(instance: SprintGameCard, e: KeyboardEventInit) {
         if (j < wordsOnPage.length) {
-          instance.node.querySelector('.word-translation').innerHTML = shuffled[j].wordTranslate;
+          instance.node.querySelector('.word-translation').innerHTML = (correctArr.includes(wordsOnPage[j])) ? wordsOnPage[j].wordTranslate : shuffled[j].wordTranslate;
           instance.node.querySelector('.word-name').innerHTML = wordsOnPage[j].word;
-          if (word.classList.contains('true-button') || e.key === 'ArrowRight') {
-            if (wordsOnPage[j].wordTranslate === shuffled[j].wordTranslate) {
-              instance.trueWord.push(wordsOnPage[j]);
-              instance.colorIndicator(true);
-              instance.seriesAns += 1;
-              if (instance.seriesAns >= 4) {
-                instance.result += 20;
-              } else instance.result += 10;
+          if (word.classList.contains('true-button')|| e.key === 'ArrowRight') {
+            if (correctArr.includes(wordsOnPage[j])) {
+            instance.trueWord.push(wordsOnPage[j]);
+            instance.colorIndicator(true);
+            instance.seriesAns += 1;
+            if (instance.seriesAns >= 4) {
+              instance.result += 20;
+            } else instance.result += 10;
             }
-            instance.result -= 10;
-            instance.seriesAns = 0;
-            instance.colorIndicator(false);
-            instance.falseWord.push(wordsOnPage[j]);
+          instance.result -= 10;
+          instance.seriesAns = 0;
+          instance.colorIndicator(false);
+          instance.falseWord.push(wordsOnPage[j]);
           }
           if (word.classList.contains('false-button') || e.key === 'ArrowLeft') {
-            if (wordsOnPage[j].wordTranslate === shuffled[j].wordTranslate) {
+            if (correctArr.includes(wordsOnPage[j])) {
               instance.falseWord.push(wordsOnPage[j]);
               instance.colorIndicator(false);
               instance.result -= 10;
@@ -110,6 +119,7 @@ export default class SprintGameCard extends Control {
             }
             instance.colorIndicator(true);
             instance.trueWord.push(wordsOnPage[j]);
+            
             if (instance.seriesAns >= 4) {
               instance.result += 20;
             } else instance.result += 10;
@@ -134,9 +144,12 @@ export default class SprintGameCard extends Control {
     this.node.querySelector('.result').innerHTML = `${this.result}`;
     this.node.querySelector('.point-result').innerHTML = `${this.result}`;
     this.node.querySelector('.right-count').innerHTML = `${this.trueWord.length}`;
-    this.node.querySelector('.right-cont').innerHTML = '';
+    this.node.querySelector('.right-cont').innerHTML = ''; 
+    
     for (let i = 0; i < this.trueWord.length; i++) {
       const rightWordCont = new Control(this.node.querySelector('.right-cont'), 'div', 'word-cont');
+      const playButton = new Control(rightWordCont.node, 'button', 'play-button');
+      playButton.node.addEventListener('click', () =>  this.playAudio(this.trueWord, i));
       new Control(rightWordCont.node, 'div', 'word', `${this.trueWord[i].word} - `);
       new Control(rightWordCont.node, 'div', 'translate', `${this.trueWord[i].wordTranslate}`);
     }
@@ -144,8 +157,33 @@ export default class SprintGameCard extends Control {
     this.node.querySelector('.wrong-cont').innerHTML = '';
     for (let i = 0; i < this.falseWord.length; i++) {
       const wrongWordCont = new Control(this.node.querySelector('.wrong-cont'), 'div', 'word-cont');
+      const playButton = new Control(wrongWordCont.node, 'button', 'play-button');
+      playButton.node.addEventListener('click', () =>  this.playAudio(this.falseWord, i));
       new Control(wrongWordCont.node, 'div', 'word', `${this.falseWord[i].word} - `);
       new Control(wrongWordCont.node, 'div', 'translate', ` ${this.falseWord[i].wordTranslate}`);
+    }
+  }
+
+  private playAudio(arr: Array<ISprint>, i:number): void {
+    if (!this.isPlaying || this.audio?.ended) {
+      this.audio = new Audio();
+      this.audio.src = `${this.serverURL}${arr[i].audio}`;
+      this.audio.play();
+      this.isPlaying = true;
+      this.audio.addEventListener('ended', () => this.getSongNext);
+    } else if (this.isPlaying) {
+      this.audio.pause();
+      this.isPlaying = false;
+    }
+  }
+
+  private getSongNext(arr: Array<ISprint>, i:number): void {
+    this.isPlaying = !this.isPlaying;
+    if (this.playNum != 0) {
+      this.playNum++;
+      this.playAudio(arr, i);
+    } else {
+      this.playNum = 0;
     }
   }
 
@@ -175,6 +213,8 @@ export default class SprintGameCard extends Control {
 
   private startTime() {
     (this.node.querySelector('.card-translate') as HTMLElement).style.display = 'none';
+    (this.node.querySelector('.false-button') as HTMLButtonElement).disabled = true;
+    (this.node.querySelector('.true-button') as HTMLButtonElement).disabled = true;
     let timeToStart = this.node.querySelector('.time-to-start');
     let interval: NodeJS.Timer,
       isStart = false;
@@ -189,6 +229,8 @@ export default class SprintGameCard extends Control {
       }, 1000);
     }
     setTimeout(() => {
+      (this.node.querySelector('.false-button') as HTMLButtonElement).disabled = false;
+      (this.node.querySelector('.true-button') as HTMLButtonElement).disabled = false;
       (this.node.querySelector('.card-start-time') as HTMLElement).style.display = 'none';
       (this.node.querySelector('.card-translate') as HTMLElement).style.display = 'flex';
       this.timer();
